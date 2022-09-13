46e140982d23d1c81c850d6b6d69ff21f9abd41f
svendenotter <sven.den.otter@rebilly.com>
2022-09-13 18:47:27 +0900

    Add Flask app

---
+++ apps/flask/app.py
@@ -0,0 +1,14 @@
+"""
+DevOps Demo Flask Application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def dev_ops_demo():
+    """Index route"""
+    return "DevOps Demo"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=1123, debug=True)
