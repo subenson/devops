80e1501f2d62da4823726055ec212c32008da2b0
svendenotter <sven.den.otter@rebilly.com>
2022-11-02 20:12:20 +0900

    Bump version

--- apps/flask/app.py
+++ apps/flask/app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def dev_ops_demo():
     """Index route."""
-    return "DevOps Demo v1.1"
+    version = open("version", "r").read()
+
+    return f"DevOps Demo {version}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1235, debug=True)
