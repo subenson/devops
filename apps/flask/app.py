a3c8b5087c016c9a1473df45a31cfe05e06519e7
svendenotter <sven.den.otter@rebilly.com>
2022-11-02 20:01:20 +0900

    Pass pylint

--- apps/flask/app.py
+++ apps/flask/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def dev_ops_demo():
-    return $
+    return "DevOps Demo v1.1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1235, debug=True)
