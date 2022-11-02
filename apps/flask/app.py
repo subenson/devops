84a6d07ae7f08039ab0e22ba4e5c2beb9e7f636e
svendenotter <sven.den.otter@rebilly.com>
2022-11-02 20:28:39 +0900

    Rolling Deployment readiness probe

--- apps/flask/app.py
+++ apps/flask/app.py
@@ -12,7 +12,7 @@ def dev_ops_demo():
     with open("version", encoding="utf-8") as version_file:
         version = version_file.read()
 
-    return f"DevOps Demo {version}"
+    return f"DevOps Demo V{version}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1235, debug=True)
